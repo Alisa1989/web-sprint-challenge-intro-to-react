e30549d7d6297df2ef41492e701e1faeb6b64ec4
alexandre steinhauslin <a.steinhauslin@gmail.com>
2020-06-26 18:22:15 -0700

    data retrieved and displayed correctly from API

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+
 import './App.css';
+import Character from "./components/Character"
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+  
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character`)
+      .then(response => {
+        console.log("Response from useEffect of App.js", response);
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.log("Error occured in useEffect of App.js: ", error);
+      });
+  }, []);
+  
+  //console.log(characters.results)
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className="characters">
+        {characters.map(char => {
+          return (
+            <Character 
+              name= {char.name}
+              status= {char.status}
+              species= {char.species}
+              gender= {char.gender}
+              origin= {char.origin.name}
+              image= {char.image}
+              url= {char.url}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// Try to think through what state you'll need for this app before starting. Then build out
+// the state properties here.
+//<Character title={characters.results.name}/>
+
+// Fetch characters from the API in an effect hook. Remember, anytime you have a 
+// side effect in a component, you want to think about which state and/or props it should
+// sync up with, if any.
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+
+import React from "react";
+
+const Character = (props) => {
+    return(
+        <div>
+            <img src= {props.image}/>
+            <h1>Name: {props.name} </h1>
+            <p>
+                <span>Status: {props.status}</span>
+                <span>Planet of Origin: {props.origin}</span>
+            </p>
+            <p>
+                <span>Species: {props.species}</span>
+                <span>Gender: {props.gender}</span>
+            </p>
+            <p>Website Page: {props.url}</p>            
+
+        </div>
+    );
+};
+
+export default Character;
